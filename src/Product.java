c42f45368e1ee7347e05c247ea14f4ffd5911467
Jonathan Rajaratnam <joeraj08@gmail.com>
2023-11-19 12:05:45 +0530

    Added variables to Clothing

--- src/Product.java
+++ src/Product.java
@@ -4,4 +4,37 @@ public abstract   class  Product {
     private String productName;
     private int noOfAvailableItems;
     private double price;
+
+    public Product(String productID, String productName, int noOfAvailableItems, double price) {
+        this.productID = productID;
+        this.productName = productName;
+        this.noOfAvailableItems = noOfAvailableItems;
+        this.price = price;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getNoOfAvailableItems() {
+        return noOfAvailableItems;
+    }
+
+    public void setNoOfAvailableItems(int noOfAvailableItems) {
+        this.noOfAvailableItems = noOfAvailableItems;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
 }
--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
 public class ShoppingCart {
+
+    ArrayList<Product> cart = new ArrayList<Product>();
+
+    public void addProduct(Product p){
+        cart.add(p);
+    }
+
+    public void removeProduct(Product p){
+        cart.remove(p);
+    }
+
+    public void calculateTotal(){
+        double total = 0;
+        for(Product p : cart){
+            total += p.getPrice();
+        }
+        System.out.println("Total: " + total);
+    }
 }
