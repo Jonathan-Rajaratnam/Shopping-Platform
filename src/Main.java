695525541d5619fc03669ed38ae4e70cb88ab4a9
Jonathan Rajaratnam <joeraj08@gmail.com>
2024-01-01 13:33:38 +0530

    Added leadProductList on start of program.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         ShoppingManager shp = new WestminsterShoppingManager();
+        shp.loadProductList();
         shp.menu();
     }
 
