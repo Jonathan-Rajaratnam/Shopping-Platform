10bbc0bb07d6a75a9f5ba6c8bb60d2591eca7aad
Jonathan Rajaratnam <joeraj08@gmail.com>
2024-01-10 07:57:11 +0530

    Added the getInstance method.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        ShoppingManager shp = new WestminsterShoppingManager();
+        ShoppingManager shp = WestminsterShoppingManager.getInstance();
         shp.loadProductList();
         shp.menu();
     }
